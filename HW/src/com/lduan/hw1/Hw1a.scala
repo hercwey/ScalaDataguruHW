a629bc6c9bf7a6bf43b5e2255899b0a863f5ee2e
lduan <duanliandba@gmail.com>
2014-05-23 09:19:32 -0400

    add h1a

---
+++ HW/src/com/lduan/hw1/Hw1a.scala
@@ -0,0 +1,25 @@
+package com.lduan.hw1
+
+class Hw1a {
+}
+object Hw1a {
+  def main(args: Array[String]) {
+    println("Hw1a");
+    val one = adder(5, 6)
+    println("one " + one);
+    val two = adder(5, one)
+    println("two " + two);
+    val three = min(one, two)
+    println(three)
+  }
+
+  def adder(m: Int, n: Int) = m & n + 3
+  def min(m: Int, n: Int): Int = {
+    var a = m + 1
+    if (m > n) a = n + 1
+    return a
+  }
+
+}
+
+
