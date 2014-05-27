fa805a4048bef50556dba8188b1c9710f986218c
lduan_home <duanliandba@gmail.com>
2014-05-26 21:40:08 -0400

    add more packages in one class

---
+++ HW/src/com/lduan/hw1/Question1.scala
@@ -0,0 +1,23 @@
+package com.lduan.hw1 {
+  package week1 {
+    class Question1 {
+    }
+
+    object Question1 {
+      def adder(m: Int, n: Int) = m & n + 3
+      def min(m: Int, n: Int): Int = {
+        var a = m + 1
+        if (m > n)
+          a = n + 1
+        return a
+      }
+
+      def main(args: Array[String]): Unit = {
+        val one = adder(5, 6)
+        val two = adder(5, one)
+        val three = min(one, two)
+        println(three)
+      }
+    }
+  }
+}
