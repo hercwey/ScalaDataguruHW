1d5c48a810b2aca20b51c59eef72133f5be12145
lduan <duanliandba@gmail.com>
2014-06-03 08:18:49 -0400

    add hw2

--- HW/src/com/lduan/hw2/Hw2.scala
+++ HW/src/com/lduan/hw2/Hw2.scala
@@ -1,14 +1,22 @@
 package com.lduan.hw2
 
+
 class Hw2 {}
 
 object Hw2 {
   def main(args: Array[String]) {
-    val m=Map(1->"a",3->"c",4->"d",5->"e",2->"b")
-    var t = scala.collection.immutable.TreeMap[Int,String]()
-      t ++= m
-      println(m);
-    print(t);
+    val m = Map(1 -> "a", 3 -> "c", 4 -> "d", 5 -> "e", 2 -> "b")
+    var t = scala.collection.immutable.TreeMap[Int, String]()
+    t ++= m
+    println(m);
+    println(t);
+    var t2 = scala.collection.mutable.Map[Int, String]()
+    t2 ++= m
+    t2 += (6 -> "f", 7 -> "g", 8 -> "h")
+    println(t2);
+    t2(1) = "aa"
+    t2(2) = "bb"
+    println(t2);
   }
 
 }
---
+++ HW/src/com/lduan/hw2/Hw3.scala
@@ -0,0 +1,25 @@
+package com.lduan.hw2
+
+import scala.collection.mutable.ArrayBuffer
+
+class Hw3 {}
+
+object Hw3 {
+  def main(args: Array[String]) {
+    var arrays = Array(1, 2, 3, 4, 1, 2)
+    println(checkArray(arrays))
+  }
+  def checkArray(a: Array[Int]) = {
+    var m = scala.collection.mutable.Map[Int, Int]()
+    var buf = new ArrayBuffer[Int]()
+    for (a1 <- a) {
+      if (m.contains(a1)) {
+        buf += (a1 + 1)
+      } else {
+        buf += a1
+        m+=(a1->a1)
+      }
+    }
+    buf
+  }
+}
