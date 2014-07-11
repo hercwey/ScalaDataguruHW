5d0054f29c764f512876e1575651b7b364100967
lduan <duanliandba@gmail.com>
2014-07-11 16:49:09 -0400

    test

--- HW/src/com/lduan/hw5/RunningMultipleCalcs.scala
+++ HW/src/com/lduan/hw5/RunningMultipleCalcs.scala
@@ -6,15 +6,21 @@ import scala.util.{ Failure, Success }
 import scala.util.Random
 object Hw11RunningMultipleCalcs extends App {
   println("starting futures")
-  val result1 = Future {createArray(3333) }
+  val result1 = Future { createArray(3333) }
   val result2 = Future { createArray(3333) }
   val result3 = Future { createArray(3333) }
+
   println("before for-comprehension")
+
+  val f1 = result1 map { x:Array[Int] => x}
+  val f2 = result1 map { x:Array[Int] => x}
+  val f3 = result1 map { x => x.clone }
+  
   val result = for {
-    r1 <- result1
-    r2 <- result2
-    r3 <- result3
-  } yield (r1 ++ r2 ++ r3)
+    r1 <- f1
+    r2 <- f2
+    r3 <- f3
+  } yield ()
  
   println("before onSuccess")
   result onSuccess {
@@ -23,7 +29,7 @@ object Hw11RunningMultipleCalcs extends App {
   println("before sleep at the end")
 
   Thread.sleep(2000) // keep the jvm alive
-    def createArray(count: Int): Array[Int] = {
+  def createArray(count: Int): Array[Int] = {
     val myArray = new Array[Int](count)
     var sums: Int = 0;
     for (i <- 0 to (count - 1)) {
